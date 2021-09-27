1472c39b261f8825a9df1944a9d85416e3a81346
TConk <>
2021-09-27 19:40:23 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,48 @@
+import RPi.GPIO as GPIO
+from time import sleep
+
+#Setup the input ports
+led1, led2, led3 = 5, 6, 13
+in1, in2 = 16, 20
+f = 1 #frequency = 1
+
+#Setup the ports
+GPIO.setmode(GPIO.BCM)
+
+#Setup the outputs
+GPIO.setup(led1, GPIO.OUT)
+GPIO.setup(led2, GPIO.OUT)
+GPIO.setup(led3, GPIO.OUT)
+
+#Setup the inputs
+GPIO.setup(in1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(in2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+#Setup the PWM For outputs
+pwmLED1 = GPIO.PWM(led1, 1)
+pwmLED2 = GPIO.PWM(led2, 1)
+pwmLED3 = GPIO.PWM(led3, 1)
+
+while True:
+  try:
+    #make the led1 or led2 blink
+    def blinkLight(channel):
+      channel.start(0)
+      for dc in range(101):
+        channel.ChangeDutyCycle(dc)
+        sleep(.01)
+      
+    #LED 1 Blinks
+    GPIO.add_event_detect(in1, GPIO.RISING, callback = blinkLight(pwmLED1), bouncetime = 1000)
+
+    #LED 2 Blinks
+    GPIO.add_event_detect(in2, GPIO.RISING, callback = blinkLight(pwmLED2), bouncetime = 1000)
+
+    blinkLight(pwmLED3)
+
+    #GPIO Cleanup
+    GPIO.cleanup()
+  except KeyboardInterrupt:
+    print('/nExiting')
+  except Exception as e:
+    print('\n'+e)
